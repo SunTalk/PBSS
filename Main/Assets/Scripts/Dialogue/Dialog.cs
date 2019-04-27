1a59be589a5d267eab04371b04c4e1678ef74e4c
Yangshihan <superman1230000@gmail.com>
2019-04-27 15:24:19 +0800

    Merge Dialogue

---
+++ Main/Assets/Scripts/Dialogue/Dialog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    //GameObject 
+    public static string CollidResever;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Talker.instace.isTalking == false)
+            {
+                Talker.instace.movebackTalkDia();
+                Talker.instace.isTalking = true;
+                CollidResever = this.name.ToString();
+            }
+        }
+    }
+}
